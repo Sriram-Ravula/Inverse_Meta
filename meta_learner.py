f0e5a48ab22f9d800f6635f2234ecf33e711ccff
Sriram Ravula <sriram.ravula@utexas.edu>
2022-01-19 14:17:58 -0600

    adding to meta runner inits

--- meta_learner.py
+++ meta_learner.py
@@ -1,3 +1,87 @@
 import torch
+from torch.utils.data import DataLoader
 import numpy as np
 from tqdm import tqdm
+import argparse
+import yaml
+
+from utils import dict2namespace, split_dataset
+from loss_utils import get_A
+
+from ncsnv2.models.ncsnv2 import NCSNv2Deeper, NCSNv2, NCSNv2Deepest
+from ncsnv2.models import get_sigmas
+from ncsnv2.models.ema import EMAHelper
+from ncsnv2.datasets import get_dataset
+
+
+class MetaLearner:
+    """
+    Meta Learning for inverse problems
+    """
+
+    def __init__(self, hparams, args):
+        self.hparams = hparams
+        self.args = args
+        self.__init_net()
+        self.__init_datasets()
+        return
+    
+    def __init_net(self):
+        if self.hparams.net.model != "ncsnv2":
+            raise NotImplementedError #TODO implement other models!
+
+        ckpt_path = self.hparams.net.checkpoint_dir
+        config_path = self.hparams.net.config_file
+
+        with open(config_path, 'r') as f:
+            config = yaml.safe_load(f)
+        net_config = dict2namespace(config)
+        net_config.device = self.hparams.device
+
+        states = torch.load(ckpt_path, map_location=self.hparams.device)
+
+        if self.hparams.data.dataset == 'ffhq':
+            test_score = NCSNv2Deepest(net_config).to(self.hparams.device)
+        elif self.hparams.data.dataset == 'celeba':
+            test_score = NCSNv2(net_config).to(self.hparams.device)
+        
+        test_score = torch.nn.DataParallel(test_score)
+        test_score.load_state_dict(states[0], strict=True)
+
+        if net_config.model.ema:
+            ema_helper = EMAHelper(mu=net_config.model.ema_rate)
+            ema_helper.register(test_score)
+            ema_helper.load_state_dict(states[-1])
+            ema_helper.ema(test_score)
+
+        test_score.eval()
+        for param in test_score.parameters():
+            param.requires_grad = False
+
+        self.model = test_score
+        self.sigmas = get_sigmas(net_config).cpu()
+        self.model_config = net_config
+        return 
+    
+    def __init_datasets(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--data_path', type=str, default='exp', help='Path where data is located')
+        args = parser.parse_args(["--data_path", self.hparams.data.data_path])
+
+        _, base_dataset = get_dataset(args, self.model_config)
+
+        train_dataset, val_dataset, test_dataset = split_dataset(base_dataset, self.hparams)
+
+        self.train_loader = DataLoader(train_dataset, batch_size=self.hparams.data.train_batch_size, shuffle=True,
+                                num_workers=2, drop_last=False)
+        self.val_loader = DataLoader(val_dataset, batch_size=self.hparams.data.val_batch_size, shuffle=False,
+                                num_workers=2, drop_last=False)
+        self.test_loader = DataLoader(test_dataset, batch_size=self.hparams.data.val_batch_size, shuffle=False,
+                                num_workers=2, drop_last=False)
+
+        return
+    
+    def __init_problem(self):
+        self.A = get_A(HPARAMS)
+
+        
