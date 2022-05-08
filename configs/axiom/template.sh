f0e62f9c383c193a5a22dc01c6d2d3564619ecee
Sriram Ravula <sriram.ravula@utexas.edu>
2022-05-08 18:48:27 -0500

    Adding configs for new experiments

---
+++ configs/axiom/template.sh
@@ -0,0 +1,7 @@
+cd /home/sravula
+
+source venvs/basic/bin/activate
+
+cd Inverse_Meta
+
+python3 main_new.py --config --doc
---
+++ configs/dgx1/template.sh
@@ -0,0 +1,7 @@
+cd /home/sr39533
+
+source venvs/inverse_meta/bin/activate
+
+cd Inverse_Meta
+
+python3 main_new.py --config --doc
