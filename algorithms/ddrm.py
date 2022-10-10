2cc604ba424d23f92203ff00dc35e2904aaac899
Sriram Ravula <sriram.ravula@utexas.edu>
2022-10-10 13:03:32 -0500

    Adding back DP for NcsnPP loading

--- algorithms/ddrm.py
+++ algorithms/ddrm.py
@@ -126,7 +126,7 @@ class DDRM(torch.nn.Module):
 
         model = NCSNpp(self.hparams)
         model = model.to(self.device)
-        # model = torch.nn.DataParallel(model)
+        model = torch.nn.DataParallel(model) #must put this here since the checkpoints are saved as DP
 
         ema = ExponentialMovingAverage(model.parameters(), decay=self.hparams.model.ema_rate)
         state = dict(model=model, ema=ema)
@@ -141,7 +141,7 @@ class DDRM(torch.nn.Module):
             param.requires_grad = False
         model.eval()
 
-        self.model = model
+        self.model = model.module.to(self.device)
 
         # if self.hparams.gpu_num == -1:
         #     self.model = model
