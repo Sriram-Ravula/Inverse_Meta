397e730676b7071b7e86b5ce7fef84d009909b15
Sriram Ravula <sriram.ravula@utexas.edu>
2022-10-13 15:08:26 -0500

    Latest

--- 1D_tests.sh
+++ 1D_tests.sh
@@ -1,4 +1,4 @@
-python3 main_new.py --config /home/sravula/Inverse_Meta/configs/new_tests/1D_Hard_Poisson_R4.yml --doc TEST_1D_Hard_Poisson_R4 --eta 0.8 --etaB 0.9 --timesteps 1000
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/new_tests/1D_Hard_Poisson_R4.yml --doc TEST_SIGMA0_1D_Hard_Poisson_R4 --eta 0.8 --etaB 0.9 --timesteps 1000
 
 python3 main_new.py --config /home/sravula/Inverse_Meta/configs/new_tests/1D_Hard_Poisson_R10.yml --doc TEST_1D_Hard_Poisson_R10 --eta 0.8 --etaB 0.9 --timesteps 1000
 
