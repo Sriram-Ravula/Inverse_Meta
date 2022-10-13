8efd00c687254113884d277787103c0f097f3819
Sriram Ravula <sriram.ravula@utexas.edu>
2022-10-13 18:08:40 -0500

    Check passes for new SSIM and PSNR minus range

--- 1D_tests.sh
+++ 1D_tests.sh
@@ -14,3 +14,6 @@ python3 main_new.py --config /home/sravula/Inverse_Meta/configs/new_tests/1D_Sof
 python3 main_new.py --config /home/sravula/Inverse_Meta/configs/new_tests/1D_Soft_Random_R4.yml --doc TEST_1D_Soft_Random_R4 --eta 0.8 --etaB 0.9 --timesteps 1000
 
 python3 main_new.py --config /home/sravula/Inverse_Meta/configs/new_tests/1D_Soft_Random_R10.yml --doc TEST_1D_Soft_Random_R10 --eta 0.8 --etaB 0.9 --timesteps 1000
+
+#REMOVED RANGE STUFF
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/new_tests/1D_Hard_Poisson_R4.yml --doc FIX_RANGE --eta 0.8 --etaB 0.9 --timesteps 1000
