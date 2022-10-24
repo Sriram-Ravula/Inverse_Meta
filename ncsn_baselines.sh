e910050ef2bbdb84680e60facaf0de4a5c85b4b0
Sriram Ravula <sriram.ravula@utexas.edu>
2022-10-23 22:41:03 -0500

    New baselines fo ncsn

---
+++ ncsn_baselines.sh
@@ -0,0 +1,11 @@
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/ncsnv2/NCSN_BASE_2D_R2.yml --doc NCSN_BASE_2D_R2 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/ncsnv2/NCSN_BASE_2D_R4.yml --doc NCSN_BASE_2D_R4 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/ncsnv2/NCSN_BASE_2D_R6.yml --doc NCSN_BASE_2D_R6 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/ncsnv2/NCSN_BASE_2D_R8.yml --doc NCSN_BASE_2D_R8 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/ncsnv2/NCSN_BASE_2D_R10.yml --doc NCSN_BASE_2D_R10 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/ncsnv2/NCSN_BASE_2D_R12.yml --doc NCSN_BASE_2D_R12 --test
