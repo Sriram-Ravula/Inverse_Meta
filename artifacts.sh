417eccd5083e186b8c890ca3170c8b31a678cde9
Sriram Ravula <sriram.ravula@utexas.edu>
2023-01-09 22:09:08 -0600

    debugging exps

---
+++ artifacts.sh
@@ -0,0 +1,28 @@
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R12_1D_0.1.yml --doc NCSN_R12_1D_01 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R12_1D_0.2.yml --doc NCSN_R12_1D_02 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R12_1D_0.5.yml --doc NCSN_R12_1D_05 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R12_1D_1.yml --doc NCSN_R12_1D_1 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R12_1D_2.yml --doc NCSN_R12_1D_2 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R12_1D_5.yml --doc NCSN_R12_1D_5 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R12_1D_10.yml --doc NCSN_R12_1D_10 --test
+
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R12_1D_0.001.yml --doc WAV_R12_1D_0001 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R12_1D_0.002.yml --doc WAV_R12_1D_0002 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R12_1D_0.005.yml --doc WAV_R12_1D_0005 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R12_1D_0.01.yml --doc WAV_R12_1D_001 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R12_1D_0.02.yml --doc WAV_R12_1D_002 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R12_1D_0.05.yml --doc WAV_R12_1D_005 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R12_1D_0.1.yml --doc WAV_R12_1D_01 --test
