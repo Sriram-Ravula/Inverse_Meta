912a4c34791f7e3810d58ec5788c4dbcbf2c88c1
Sriram Ravula <sriram.ravula@utexas.edu>
2023-01-12 22:24:01 -0600

    Latest exps

---
+++ 1D_ncsn_base.sh
@@ -0,0 +1,12 @@
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R4_HOR_1e0.yml --doc NCSN_R4_HOR_1e0 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R4_HOR_2e0.yml --doc NCSN_R4_HOR_2e0 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R4_HOR_5e-1.yml --doc NCSN_R4_HOR_5e-1 --test
+
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R4_VER_1e0.yml --doc NCSN_R4_VER_1e0 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R4_VER_2e0.yml --doc NCSN_R4_VER_2e0 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/NCSN_R4_VER_5e-1.yml --doc NCSN_R4_VER_5e-1 --test
---
+++ wavelet_base.sh
@@ -0,0 +1,28 @@
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_HOR_1e-1.yml --doc WAV_R4_HOR_1e-1 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_HOR_1e-2.yml --doc WAV_R4_HOR_1e-2 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_HOR_1e-3.yml --doc WAV_R4_HOR_1e-3 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_HOR_2e-2.yml --doc WAV_R4_HOR_2e-2 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_HOR_2e-3.yml --doc WAV_R4_HOR_2e-3 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_HOR_5e-2.yml --doc WAV_R4_HOR_5e-2 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_HOR_5e-3.yml --doc WAV_R4_HOR_5e-3 --test
+
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_VER_1e-1.yml --doc WAV_R4_VER_1e-1 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_VER_1e-2.yml --doc WAV_R4_VER_1e-2 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_VER_1e-3.yml --doc WAV_R4_VER_1e-3 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_VER_2e-2.yml --doc WAV_R4_VER_2e-2 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_VER_2e-3.yml --doc WAV_R4_VER_2e-3 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_VER_5e-2.yml --doc WAV_R4_VER_5e-2 --test
+
+python3 main_new.py --config /home/sravula/Inverse_Meta/configs/debug_artifacts/WAV_R4_VER_5e-3.yml --doc WAV_R4_VER_5e-3 --test
