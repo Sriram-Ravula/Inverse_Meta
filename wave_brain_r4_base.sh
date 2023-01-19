9298b8c3814b4a7d48213b9e6e52ceaf94038864
Sriram Ravula <sriram.ravula@utexas.edu>
2023-01-18 23:38:59 -0600

    Adding latest experiments

---
+++ wave_brain_r4_base.sh
@@ -0,0 +1,9 @@
+python3 main_new.py --config "/home/sravula/Inverse_Meta/configs/wavelet_brain_baselines/WAVE_BASE_R4_HOR_1e-3.yml" --doc WAVE_R4_HOR_1e-3_BASE --test
+
+python3 main_new.py --config "/home/sravula/Inverse_Meta/configs/wavelet_brain_baselines/WAVE_BASE_R4_HOR_2e-3.yml" --doc WAVE_R4_HOR_2e-3_BASE --test
+
+python3 main_new.py --config "/home/sravula/Inverse_Meta/configs/wavelet_brain_baselines/WAVE_BASE_R4_HOR_5e-3.yml" --doc WAVE_R4_HOR_5e-3_BASE --test
+
+python3 main_new.py --config "/home/sravula/Inverse_Meta/configs/wavelet_brain_baselines/WAVE_BASE_R4_HOR_1e-2.yml" --doc WAVE_R4_HOR_1e-2_BASE --test
+
+python3 main_new.py --config "/home/sravula/Inverse_Meta/configs/wavelet_brain_baselines/WAVE_BASE_R4_HOR_2e-2.yml" --doc WAVE_R4_HOR_2e-2_BASE --test
---
+++ wave_brain_r8_base.sh
@@ -0,0 +1,9 @@
+python3 main_new.py --config "/home/sravula/Inverse_Meta/configs/wavelet_brain_baselines/WAVE_BASE_R8_HOR_1e-3.yml" --doc WAVE_R8_HOR_1e-3_BASE --test
+
+python3 main_new.py --config "/home/sravula/Inverse_Meta/configs/wavelet_brain_baselines/WAVE_BASE_R8_HOR_2e-3.yml" --doc WAVE_R8_HOR_2e-3_BASE --test
+
+python3 main_new.py --config "/home/sravula/Inverse_Meta/configs/wavelet_brain_baselines/WAVE_BASE_R8_HOR_5e-3.yml" --doc WAVE_R8_HOR_5e-3_BASE --test
+
+python3 main_new.py --config "/home/sravula/Inverse_Meta/configs/wavelet_brain_baselines/WAVE_BASE_R8_HOR_1e-2.yml" --doc WAVE_R8_HOR_1e-2_BASE --test
+
+python3 main_new.py --config "/home/sravula/Inverse_Meta/configs/wavelet_brain_baselines/WAVE_BASE_R8_HOR_2e-2.yml" --doc WAVE_R8_HOR_2e-2_BASE --test
