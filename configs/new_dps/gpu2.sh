dba3698b64bc259895597576cd5993fa5987af3a
Sriram Ravula <sriram.ravula@utexas.edu>
2023-05-09 13:22:25 -0500

    Removing unneeded configs

---
+++ configs/new_dps/gpu2.sh
@@ -0,0 +1,11 @@
+python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/new_dps/knee_3d_r4_10.yml --doc knee_3d_r4_10
+
+python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/new_dps/knee_3d_r4_25.yml --doc knee_3d_r4_25
+
+python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/new_dps/knee_3d_r4_50.yml --doc knee_3d_r4_50
+
+python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/new_dps/knee_3d_r8_10.yml --doc knee_3d_r8_10
+
+python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/new_dps/knee_3d_r8_25.yml --doc knee_3d_r8_25
+
+python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/new_dps/knee_3d_r8_50.yml --doc knee_3d_r8_50
