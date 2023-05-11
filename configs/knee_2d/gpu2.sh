13e3c516f040c679b322b1a573cd621fb1824ab0
Sriram Ravula <sriram.ravula@utexas.edu>
2023-05-11 15:42:32 -0500

    Latest from Danube

--- configs/knee_2d/gpu2.sh
+++ configs/knee_2d/gpu2.sh
@@ -1,16 +1,16 @@
-python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-L2-100.yml --doc Knee_2D_R8_1-L2-100
+# python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-L2-100.yml --doc Knee_2D_R8_1-L2-100
 
-python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-L2-200.yml --doc Knee_2D_R8_1-L2-200
+# python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-L2-200.yml --doc Knee_2D_R8_1-L2-200
 
-python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-L2-500.yml --doc Knee_2D_R8_1-L2-500
+# python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-L2-500.yml --doc Knee_2D_R8_1-L2-500
 
-python3 main_new_2_1.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-SSIM-100.yml --doc Knee_2D_R8_1-SSIM-100
+# python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-SSIM-100.yml --doc Knee_2D_R8_1-SSIM-100
 
-python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-SSIM-200.yml --doc Knee_2D_R8_1-SSIM-200
+# python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-SSIM-200.yml --doc Knee_2D_R8_1-SSIM-200
 
-python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-SSIM-500.yml --doc Knee_2D_R8_1-SSIM-500
+# python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_1-SSIM-500.yml --doc Knee_2D_R8_1-SSIM-500
 
-python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_2-L2-100.yml --doc Knee_2D_R8_2-L2-100
+# python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_2-L2-100.yml --doc Knee_2D_R8_2-L2-100
 
 python3 main_new_2.py --config /home/sravula/Inverse_Meta/configs/knee_2d/Knee_2D_R8_2-L2-200.yml --doc Knee_2D_R8_2-L2-200
 
--- configs/knee_3d/gpu3.sh
+++ configs/knee_3d/gpu3.sh
@@ -1,16 +1,16 @@
-python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-L2-100.yml --doc Knee_3D_R8_1-L2-100
+# python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-L2-100.yml --doc Knee_3D_R8_1-L2-100
 
-python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-L2-200.yml --doc Knee_3D_R8_1-L2-200
+# python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-L2-200.yml --doc Knee_3D_R8_1-L2-200
 
-python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-L2-500.yml --doc Knee_3D_R8_1-L2-500
+# python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-L2-500.yml --doc Knee_3D_R8_1-L2-500
 
-python3 main_new_3_1.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-SSIM-100.yml --doc Knee_3D_R8_1-SSIM-100
+# python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-SSIM-100.yml --doc Knee_3D_R8_1-SSIM-100
 
-python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-SSIM-200.yml --doc Knee_3D_R8_1-SSIM-200
+# python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-SSIM-200.yml --doc Knee_3D_R8_1-SSIM-200
 
-python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-SSIM-500.yml --doc Knee_3D_R8_1-SSIM-500
+# python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_1-SSIM-500.yml --doc Knee_3D_R8_1-SSIM-500
 
-python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_2-L2-100.yml --doc Knee_3D_R8_2-L2-100
+# python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_2-L2-100.yml --doc Knee_3D_R8_2-L2-100
 
 python3 main_new_3.py --config /home/sravula/Inverse_Meta/configs/knee_3d/Knee_3D_R8_2-L2-200.yml --doc Knee_3D_R8_2-L2-200
 
